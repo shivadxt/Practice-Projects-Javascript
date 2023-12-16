64214d01a96fc1121b5820b101be3a8dea963ed1
shivadxt <shivamweb3.0@gmail.com>
2023-12-16 15:56:15 +0530

    Commit

--- Assignment-2/EJS/script.js
+++ Assignment-2/EJS/script.js
@@ -1,1 +1,16 @@
 //Learn about Ejs
+const express = require ("express");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.get('/', function(req,res){
+    res.send("Hey Budddy Light Weight")
+});
+
+app.get('/contact', function(req,res){
+    res.render("index");
+});
+
+app.listen(4001);
