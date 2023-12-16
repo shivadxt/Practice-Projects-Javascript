4c33706213adc3f50d44a0e682fa6e18a531f757
shivadxt <shivamweb3.0@gmail.com>
2023-12-16 19:28:24 +0530

    assignment-3

--- Assignment-3/Script.js
+++ Assignment-3/Script.js
@@ -4,6 +4,9 @@ const app = express();
 
 app.set("view engine", "ejs");
 
+//This is how we setup STATIC files
+app.use(express.static("./public"));
+
 app.get('/', function(req,res){
     res.render("index");
 });
